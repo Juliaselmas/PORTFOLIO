096c9c6db1600e9b2c257c8373387e351a344561
Juliaselmas <144105839+Juliaselmas@users.noreply.github.com>
2024-08-06 14:11:47 +0200

    added styling and funcionality for modals

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Just testing!");
+
+let modal = document.getElementById("modal");
+let openModalBtn = document.querySelector(".modal-button");
+let span = document.querySelector(".close");
+
+openModalBtn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
