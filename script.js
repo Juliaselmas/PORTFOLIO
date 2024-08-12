9c6476d02a9b03509bd4f32e53a2bdfb7af47075
Juliaselmas <144105839+Juliaselmas@users.noreply.github.com>
2024-08-12 12:32:58 +0200

    added javascript for modals

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
 console.log("Just testing!");
 
-let modal = document.getElementById("modal");
-let openModalBtn = document.querySelector(".modal-button");
-let span = document.querySelector(".close");
+//let modal = document.querySelector(".modal");
 
+let openModalBtn = document.querySelectorAll(".modal-button");
+let closeModalBtn = document.querySelectorAll(".close");
+
+function openModal(modalId) {
+ let modal = document.getElementById(modalId);
+ if (modal) {
+    modal.style.display = "block";
+ }
+}
+
+function closeModal(modalId) {
+    let modal = document.getElementById(modalId);
+    if (modal) {
+        modal.style.display = "none";
+    }
+}
+
+openModalBtn.forEach(button => {
+    button.addEventListener("click", () => {
+        let modalId = button.getAttribute("data-modal");
+        openModal(modalId);
+    });
+});
+
+closeModalBtn.forEach(button => {
+    button.addEventListener("click", () => {
+        let modalId = button.getAttribute("data-modal");
+        closeModal(modalId);
+    });
+});
+
+window.addEventListener("click", (event) => {
+    if (event.target.classList.contains("modal")) {
+        closeModal(event.target.id);
+    }
+});
+
+/*
 openModalBtn.onclick = function() {
     modal.style.display = "block";
 }
@@ -17,3 +53,4 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+*/
