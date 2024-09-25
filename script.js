cc52eeaed60e1cd9ee23ea678880be03949b4102
Juliaselmas <144105839+Juliaselmas@users.noreply.github.com>
2024-09-25 13:28:13 +0200

    changes in layout

--- script.js
+++ script.js
@@ -6,37 +6,37 @@ let openModalBtn = document.querySelectorAll(".modal-button");
 let closeModalBtn = document.querySelectorAll(".close");
 
 function openModal(modalId) {
- let modal = document.getElementById(modalId);
- if (modal) {
+  let modal = document.getElementById(modalId);
+  if (modal) {
     modal.style.display = "block";
- }
+  }
 }
 
 function closeModal(modalId) {
-    let modal = document.getElementById(modalId);
-    if (modal) {
-        modal.style.display = "none";
-    }
+  let modal = document.getElementById(modalId);
+  if (modal) {
+    modal.style.display = "none";
+  }
 }
 
-openModalBtn.forEach(button => {
-    button.addEventListener("click", () => {
-        let modalId = button.getAttribute("data-modal");
-        openModal(modalId);
-    });
+openModalBtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    let modalId = button.getAttribute("data-modal");
+    openModal(modalId);
+  });
 });
 
-closeModalBtn.forEach(button => {
-    button.addEventListener("click", () => {
-        let modalId = button.getAttribute("data-modal");
-        closeModal(modalId);
-    });
+closeModalBtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    let modalId = button.getAttribute("data-modal");
+    closeModal(modalId);
+  });
 });
 
 window.addEventListener("click", (event) => {
-    if (event.target.classList.contains("modal")) {
-        closeModal(event.target.id);
-    }
+  if (event.target.classList.contains("modal")) {
+    closeModal(event.target.id);
+  }
 });
 
 /*
